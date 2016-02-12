b0baed629a47c56bfe31b93823133d449968f980
Nicolas Hory <hory.nicolas@etu.unice.fr>
2016-02-12 09:41:45 +0100

    Ajout folder de base dans le script (j'augmente mon nombre de commit)

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-mvn clean 
+mvn clean
 mvn validate 
-mvn compile
-mvn test 
+if [ ! -d Base ]; then
+  mkdir Base
+else echo "Base folder already exists..."
+fi
+mvn compile -DbuildDirectory=Base
+mvn test
+#for (mutation [i]) {
+#    créer un dossier mutation[i] -> code mutant généré (.class) + xml
+#}
 #xml généré 
 #html synthèse 
