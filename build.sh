72aa51cfa22893ad2ba3cf4df1f4f4b7c75d84f8
Nicolas Hory <hory.nicolas@etu.unice.fr>
2016-02-12 10:11:06 +0100

    Modification build.sh rediriger reports fail

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@ mvn clean
 mvn validate 
 if [ ! -d Base ]; then
   mkdir Base
-else echo "Base folder already exists..."
+else 
+    echo "Base folder already exists..."
 fi
 mvn compile -DbuildDirectory=Base
-mvn test
+mvn surefire-report:report -DoutputDirectory=Base/report
 #for (mutation [i]) {
 #    créer un dossier mutation[i] -> code mutant généré (.class) + xml
 #}
