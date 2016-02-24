2a39e9c7c7777aea9ade51fe2302154be0b4409f
Loïc POTAGES <pl407640@etu.unice.fr>
2016-02-24 15:17:23 +0100

    Création de la base d'éxécution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import framework.CodeTester;
+
 /**
  * @author Nicolas HORY
  * @version 12/02/16.
@@ -5,5 +7,11 @@
 public class Main {
     public String helloWorld() {
         return "Hello World !";
+    }
+
+    public static void main (String[] args)
+    {
+        CodeTester tester = new CodeTester();
+        tester.execute();
     }
 }
---
+++ src/main/java/framework/CodeTester.java
@@ -0,0 +1,13 @@
+package framework;
+
+/**
+ * Class that will execute all the operation
+ * Created by lpotages on 24/02/2016.
+ */
+public class CodeTester
+{
+    public void execute()
+    {
+        System.out.println("Execution du code");
+    }
+}
