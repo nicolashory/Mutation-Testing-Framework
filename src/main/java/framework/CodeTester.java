2bba777777a8dc9a6e0e2997ec0c5bc4488d2921
Neoskai <lpotages@gmail.com>
2016-02-25 00:14:13 +0100

    Methods in comments

--- src/main/java/framework/CodeTester.java
+++ src/main/java/framework/CodeTester.java
@@ -20,6 +20,20 @@ public class CodeTester
         // 2 eme etape: On genere les mutants sur ce code et on les stock dans des dossiers spécifique
 
         // 3 eme etape : On applique le mvn test du code source de base de chacun des dossiers
+        /*
+            Ressource:  https://maven.apache.org/surefire/maven-surefire-plugin/test-mojo.html
+
+            Pour chaque dossier créé
+            On change le classdirectory et on lance le maven test
+            Ajout de l'option redirectTestOutputToFile
+            reportFormat pour un report plus complet
+            reportsDirectory pour changer le lieu d'ecriture des reports
+            testFailureIgnore pour ignorer les echecs de tests
+         */
+
+        /*
+        Utilisation du Spoon maven plugin : https://github.com/SpoonLabs/spoon-maven-plugin
+         */
 
         // 4 eme étape:
     }
