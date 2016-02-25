c3eea576da1d72d9886c96c2e982eebbb7bc0e93
Nicolas Hory <hory.nicolas@etu.unice.fr>
2016-02-25 18:47:54 +0100

    Script qui recupere nos mutations, se déplace dans le bon dossier

--- build.sh
+++ build.sh
@@ -1,23 +1,45 @@
 #!/bin/bash
-mvn clean
-mvn validate 
-if [ ! -d Base ]; then
-  mkdir Base
-else 
-    echo "Base folder already exists..."
+#Cas d'erreur si pas de paramètres passés
+if [ ! $1 ]; then
+    echo -e "Erreur: Pas de chemin passé en paramètre  pour le code à muter..."
+    echo -e "Usage: ./build.sh cheminCodeSource"
+    exit 1
 fi
-mvn compile -DbuildDirectory=Base
-mvn test -DreportDirectory=Base/reports
-#for (mutation [i]) {
-#    créer un dossier mutation[i] -> code mutant généré (.class) + xml
-#}
-#xml généré 
-#html synthèse
-java ReportCreater
 
-# 1. On mvn install le Framework
+#Cas d'erreur si trop de paramètres passés
+if [ $2 ]; then
+    echo -e "Erreur: Trop d'arguments passés en paramètre pour le code à muter..."
+    echo -e "Usage: ./build.sh cheminCodeSource"
+    exit 1
+fi
+
+#On se place dans le dossier du framework
+cd Framework
+
+#1: On lance le maven install
+echo -e "Lancement de Maven Install pour le framework"
+mvn install
+
 # 2. Pour chaque processor dans notre framework (sauvegardés dans un fichier config?)
 #        Faire -> Editer le pom de l'application en mettant uniquement le bon processor dedant
 #        Lancer le mvn compile de l'application avec en option le buildirectory souhaité pour cette version du mutant
 #        Lancer le maven surefire test de l'application avec les bonnes options (en modifiant le dossier des sources par le buildirectory utilisé)
-# 3. Generer le rapport html a partir de tous les rapports générés
+
+#Recuperation de toutes nos mutations
+echo -e "Récupération de l'ensemble des mutations à appliquer.."
+cd src/main/java/mutation
+allMutation=($(ls | cut -f1 -d'.'))
+# Affichage de toutes les mutations : echo "${allMutation[*]}"
+# Recuperation du nombre de mutations
+nbMutation=${#allMutation[@]}
+
+cd $1
+mkdir NoMutation
+mvn compile -DbuildDirectory NoMutation
+mvn surefire test -DreportDirectory=./NoMutation/reports
+
+# 3. Generer le rapport html a partir de tous les rapports générés: java ReportCreater
+# 4. Clean l'ensemble des dossiers créés pour la génération et utilisation des mutants
+
+# Ancienne utilisation de compile: mvn compile -DbuildDirectory=Base
+# Ancienne utilisation de test: mvn test -DreportDirectory=Base/reports
