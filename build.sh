f318098473c7017fc9d0c912cadfa71545a1bf9e
Neoskai <lpotages@gmail.com>
2016-02-25 17:07:00 +0100

    Ajout de l'algorithme en commentaire dans le build shell

--- build.sh
+++ build.sh
@@ -14,3 +14,10 @@ mvn test -DreportDirectory=Base/reports
 #xml généré 
 #html synthèse
 java ReportCreater
+
+# 1. On mvn install le Framework
+# 2. Pour chaque processor dans notre framework (sauvegardés dans un fichier config?)
+#        Faire -> Editer le pom de l'application en mettant uniquement le bon processor dedant
+#        Lancer le mvn compile de l'application avec en option le buildirectory souhaité pour cette version du mutant
+#        Lancer le maven surefire test de l'application avec les bonnes options (en modifiant le dossier des sources par le buildirectory utilisé)
+# 3. Generer le rapport html a partir de tous les rapports générés
