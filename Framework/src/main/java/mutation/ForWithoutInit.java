dfc71265863046a9cfc2abef7d291a902e6f035f
Loïc POTAGES <pl407640@etu.unice.fr>
2016-03-02 14:47:20 +0100

    Création de Mutations

---
+++ Framework/src/main/java/mutation/ForWithoutInit.java
@@ -0,0 +1,34 @@
+package mutation;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.BinaryOperatorKind;
+import spoon.reflect.code.CtBinaryOperator;
+import spoon.reflect.code.CtFor;
+import spoon.reflect.code.CtStatement;
+import spoon.reflect.declaration.CtElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Mutator that edits the for increment of a program
+ * Created by lpotages on 02/03/2016.
+ */
+public class ForWithoutInit extends AbstractProcessor<CtElement>
+{
+    @Override
+    public boolean isToBeProcessed(CtElement candidate) {
+        return candidate instanceof CtFor;
+    }
+
+    @Override
+    public void process(CtElement candidate) {
+        if (!(candidate instanceof CtFor)) {
+            return;
+        }
+        CtFor op = (CtFor)candidate;
+
+        // Removes all the initialisations of a for statement
+        op.setForInit(new ArrayList<CtStatement>());
+    }
+}
