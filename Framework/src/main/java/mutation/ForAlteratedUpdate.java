8e0b7fcecb49de50b6debabc8957a17351880c71
Loïc POTAGES <pl407640@etu.unice.fr>
2016-03-02 15:06:37 +0100

    Mutation qui altere toutes les boucles for

---
+++ Framework/src/main/java/mutation/ForAlteratedUpdate.java
@@ -0,0 +1,73 @@
+package mutation;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.*;
+import spoon.reflect.declaration.CtElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Mutator that edits the for increment of a program
+ * Created by lpotages on 02/03/2016.
+ */
+public class ForAlteratedUpdate extends AbstractProcessor<CtElement>
+{
+    @Override
+    public boolean isToBeProcessed(CtElement candidate) {
+        return candidate instanceof CtFor;
+    }
+
+    @Override
+    public void process(CtElement candidate) {
+        if (!(candidate instanceof CtFor)) {
+            return;
+        }
+        CtFor op = (CtFor)candidate;
+
+        // Removes all the initialisations of a for statement
+       List<CtStatement> updates = op.getForUpdate();
+        List<CtStatement> newUpdates = new ArrayList<CtStatement>();
+
+        for(CtStatement s : updates)
+        {
+            CtCodeSnippetStatement newStatement = getFactory().Core().createCodeSnippetStatement();
+
+            String increment = "";
+
+            if (s.toString().contains("+=")) {
+                String[] str = s.toString().split("\\+");
+                increment = str[0] + "++";
+            }
+            else if (s.toString().contains("-="))
+            {
+                String[] str = s.toString().split("-");
+                increment = str[0] + "--";
+            }
+
+            else if (s.toString().contains("="))
+            {
+                String[] str = s.toString().split("=");
+                if(str[1].contains("+"))
+                    increment = str[0] + "++";
+                else
+                    increment = str[0] + "--";
+            }
+            else if (s.toString().contains("++"))
+            {
+                String[] str = s.toString().split("\\+");
+                increment = str[0] + "+=2";
+            }
+            else if (s.toString().contains("--"))
+            {
+                String[] str = s.toString().split("-");
+                increment = str[0] + "-=2";
+            }
+
+            newStatement.setValue(increment);
+            newUpdates.add(newStatement);
+        }
+
+        op.setForUpdate(newUpdates);
+    }
+}
